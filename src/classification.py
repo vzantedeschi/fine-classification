29a68718f240666d68340c24117c99a06c3f8c4b
Valentina Zantedeschi <valentina.zantedeschi@univ-st-etienne.fr>
2020-05-05 09:49:47 +0200

    minors

--- src/classification.py
+++ src/classification.py
@@ -20,6 +20,7 @@ def predict_booster(model, xs, best_iteration=True):
     return np.argmax(probs, 1)
 
 def save_model(model, save_dir):
+    # save best iteration
     model.save_model(os.path.join(save_dir, "model.txt"))
 
 def train_booster(train_xs, train_ys, val_xs, val_ys, params):
